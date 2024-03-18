d4ee1b6607964f8236ba2b67ac41fcb3be0b2380
Michi <mgruener2@gmx.de>
2024-03-18 11:48:42 +0100

    removed devtools command

--- Funcs/BuildHTML.js
+++ Funcs/BuildHTML.js
@@ -1,11 +1,5 @@
 const BuildHTML = () => {
 
-    //Adding File to Dev-Tools
-    const Overlay = document.createElement('script');
-    Overlay.src = 'https://michi19935.github.io/Warenkorb-Overlay---Git/index.js';
-    Overlay.type = 'module';
-    document.body.prepend(Overlay);
-
     //Adding CSS file to DOM
     const CustomCSS = document.createElement('link');
     CustomCSS.rel = 'stylesheet';
