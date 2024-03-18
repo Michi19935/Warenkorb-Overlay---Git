2f6b17f311f0556e68079f877970f9f72bd348f8
Michi <mgruener2@gmx.de>
2024-03-18 19:33:35 +0100

    adjusted func filter

--- Funcs/GetItems/InsertRec.js
+++ Funcs/GetItems/InsertRec.js
@@ -3,12 +3,14 @@ import Items from "./Items.js";
 const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type) => {
     let cart = JSON.parse(localStorage.getItem('cart'));
     let undefValues = 0;
+
         for(let i=0; i <ImageRec.length; i++){
+            let DuplicateProdut = cart.filter((el)=>{return el.ImageSel == ImageRec[i]})
         if(ImageRec[i] == undefined){
             undefValues++;
             continue; 
             //Checking if item is already displayed in basket
-        } else if(ImageRec[i] == cart.filter((el)=>{return el.ImageSel == ImageRec[i]})){
+        } else if(DuplicateProdut != null){
             continue; 
         } else {
             const RecItemsParam = Items(ImageRec[i],titlesRec[i],PriceRec[i],DeeplinkRec[i],Type);
