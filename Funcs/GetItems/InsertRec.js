f9b847def5bb2336bd4e4ee34904d971273f6da2
Michi <mgruener2@gmx.de>
2024-03-18 20:18:31 +0100

    moretesting

--- Funcs/GetItems/InsertRec.js
+++ Funcs/GetItems/InsertRec.js
@@ -2,19 +2,17 @@ import Items from "./Items.js";
 
 const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type) => {
     let cart = JSON.parse(localStorage.getItem('cart'));
+    console.log(cart.ImageSel);
     if(cart == null){cart = []}
     let undefValues = 0;
 
         for(let i=0; i <ImageRec.length; i++){
             let DuplicateProdut = cart.filter((el)=>{return el.ImageSel == ImageRec[i]});
-            console.log(ImageRec[i]);
+
         if(ImageRec[i] == undefined){
             undefValues++;
             continue; 
             //Checking if item is already displayed in basket
-        } else if(DuplicateProdut.length != 0){
-
-            continue; 
         } else {
             const RecItemsParam = Items(ImageRec[i],titlesRec[i],PriceRec[i],DeeplinkRec[i],Type);
             RecItemsParam.classList = 'RecItem';
