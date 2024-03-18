52d83ec655de16a86c53431371b3e28ecf2af5c1
Michi <mgruener2@gmx.de>
2024-03-18 14:45:12 +0100

    addtocartPPages ausgelagert

---
+++ Funcs/addtoCartProductPages.js
@@ -0,0 +1,17 @@
+const addtoCartProductPages = () => {
+    const addtoCartProductPages = document.querySelector('.single_add_to_cart_button');
+
+    addtoCartProductPages.addEventListener('click', ()=>{
+        const Selectoren = ['.wd-carousel-item a img','h1.product_title','p.price','.single_add_to_cart_button'];
+
+        const PImage = document.querySelector(Selectoren[0]).src;
+        const Title = document.querySelector(Selectoren[1]).innerText;
+        const Price = document.querySelector(Selectoren[2]).innerText;
+        const Id = document.querySelector(Selectoren[3]).getAttribute('value');
+        const Deeplink = window.location.href;
+
+    AddtoOverlay(PImage,Price,Deeplink,Title,'BasketItem',Id);
+    });
+}
+
+export default addtoCartProductPages
--- index.js
+++ index.js
@@ -2,21 +2,7 @@ import BuildHTML from './Funcs/BuildHTML.js';
 
 import Items from './Funcs/Items.js';
 
-const addtoCartProductPages = () => {
-    const addtoCartProductPages = document.querySelector('.single_add_to_cart_button');
-
-    addtoCartProductPages.addEventListener('click', ()=>{
-        const Selectoren = ['.wd-carousel-item a img','h1.product_title','p.price','.single_add_to_cart_button'];
-
-        const PImage = document.querySelector(Selectoren[0]).src;
-        const Title = document.querySelector(Selectoren[1]).innerText;
-        const Price = document.querySelector(Selectoren[2]).innerText;
-        const Id = document.querySelector(Selectoren[3]).getAttribute('value');
-        const Deeplink = window.location.href;
-
-    AddtoOverlay(PImage,Price,Deeplink,Title,'BasketItem',Id);
-    });
-}
+import addtoCartProductPages from './Funcs/addtoCartProductPages.js';
 
 import {GetDeeplinksRec, GetTitlesRec, GetPriceRec, GetImagesRec, alternativeImages} from './Funcs/GetFuncs.js'
 
