e837a34305905b61fde27c65e37950336cfc1df0
Michi <mgruener2@gmx.de>
2024-03-18 16:02:44 +0100

    index.js

---
+++ Funcs/LaunchOverlay.js
@@ -0,0 +1,26 @@
+//This function is used to hide and show the overlay
+const LaunchOverlay = () => {
+    document.onmouseleave = () => { 
+        let Newtimestamp = new Date().getTime();
+        //Read All Cookies from site and turn string into object and then search for OverlayViewed Cookie
+        let OldTimestamp = JSON.parse(localStorage.getItem('OldTimestamp'));
+        let DurationSinceLastViewed = new Date(Newtimestamp-OldTimestamp).getSeconds();
+        //Store current timestamp in cookie
+        localStorage.setItem('OldTimestamp', JSON.stringify(Newtimestamp));
+
+        let cart = JSON.parse(localStorage.getItem('cart'));
+        if(cart == null){
+            cart = [];
+        }
+    
+        if(DurationSinceLastViewed>=0 && cart[0] != null){
+            console.log('launch please')
+            // Set internal cookie
+                jQuery(($) => {
+                    $('#Parent').show();
+                }); 
+        }
+    };
+}
+
+export default LaunchOverlay
--- index.js
+++ index.js
@@ -3,10 +3,10 @@ import addtoCartProductPages from './Funcs/addtoCartProductPages.js';
 import addtoCartAllOtherPages from './Funcs/addtoCartAllOtherPages.js';
 import GetValuesRec from './Funcs/GetValuesRec.js';
 import GetLocalStorageItems from './Funcs/GetLocalStorageItems.js';
-
-//Warenkorb Seite Remove Item & Restore Item
 import RemoveItems from './Funcs/RemoveItems.js';
+import LaunchOverlay from './Funcs/LaunchOverlay.js';
 
+//This function is used to add and remove items to basket and recommendation sections
 const PrepareOverlay = () => { 
 
     BuildHTML();
@@ -20,36 +20,11 @@ const PrepareOverlay = () => {
     } else {
         addtoCartAllOtherPages();
     } 
-
     //Hide Overlay until it gets triggered by MouseLeave Func
     document.querySelector('#Parent').style.display = 'none';
 }
-
 PrepareOverlay();
 
-const LaunchOverlay = () => {
-    document.onmouseleave = () => { 
-        let Newtimestamp = new Date().getTime();
-        //Read All Cookies from site and turn string into object and then search for OverlayViewed Cookie
-        let OldTimestamp = JSON.parse(localStorage.getItem('OldTimestamp'));
-        let DurationSinceLastViewed = new Date(Newtimestamp-OldTimestamp).getSeconds();
-        //Store current timestamp in cookie
-        localStorage.setItem('OldTimestamp', JSON.stringify(Newtimestamp));
-
-        let cart = JSON.parse(localStorage.getItem('cart'));
-        if(cart == null){
-            cart = [];
-        }
-    
-        if(DurationSinceLastViewed>=0 && cart[0] != null){
-            console.log('launch please')
-            // Set internal cookie
-                jQuery(($) => {
-                    $('#Parent').show();
-                }); 
-        }
-    };
-}
-
+//Launch Overlay when basket >= 1 and durtions since last shown >= 10 seconds
 LaunchOverlay();
         
