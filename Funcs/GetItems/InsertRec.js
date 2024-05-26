0ca9a6eaae5dfdccdec036d388bed84c3f5f06b1
Michi <mgruener2@gmx.de>
2024-05-26 17:46:12 +0200

    undefined images rec remove

--- Funcs/GetItems/InsertRec.js
+++ Funcs/GetItems/InsertRec.js
@@ -5,7 +5,7 @@ const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type, Id) => {
     
     let undefValues = 0;
         for(let i=0; i <ImageRec.length; i++){
-        if(ImageRec[i] == undefined){
+        if(ImageRec[i] == undefined || ImageRec[i].includes('data:image/svg')){
             undefValues++;
             continue; 
             //Checking if item is already displayed in basket
