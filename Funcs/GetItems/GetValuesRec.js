f97fdcc21e1bde727b3ed86b9a8dfb6b68ca4c2c
Michi <mgruener2@gmx.de>
2024-05-26 15:21:57 +0200

    image selcetor adjusted

--- Funcs/GetItems/GetValuesRec.js
+++ Funcs/GetItems/GetValuesRec.js
@@ -8,7 +8,7 @@ const GetValuesRecommendations = () => {
     const titlesRecSelect = [...document.querySelectorAll('.product-wrapper h3 a')];
     const DeeplinkRecSelect = [...document.querySelectorAll('.product-wrapper h3 a')];
     const PriceRecSelect = [...document.querySelectorAll('.product-wrapper span.price')];
-    const ImageRecSelect = [...document.querySelectorAll('.product-wrapper img.entered.lazyloaded')];
+    const ImageRecSelect = [...document.querySelectorAll(/*'.product-wrapper img.entered.lazyloaded'*/ '.product-wrapper img')];
     const Product_id = [...document.querySelectorAll('h4')];
     //Add Product to "Recommendation-Section"
     AddtoOverlay(ImageRecSelect,PriceRecSelect,DeeplinkRecSelect,titlesRecSelect,'RecommendationItem',Product_id);
