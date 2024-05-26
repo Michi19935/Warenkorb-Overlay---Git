f015a588b945f7681b96fb5ee62c9cf7b27cd03f
Michi <mgruener2@gmx.de>
2024-05-26 15:34:51 +0200

    removed special case where rec items are hidden due to lazy loading

--- Funcs/GetItems/GetFuncs.js
+++ Funcs/GetItems/GetFuncs.js
@@ -2,31 +2,32 @@
 
 //as the RecommendationImages are lazyloaded and can only be seen after viewing them,
 //the images need to get from another source. So I am matching the titles with an alternative source of images I found
-const alternativeImages = (titles) => {
-    const ImageRecSelect = [...document.querySelectorAll('.swiper-slide-inner img')];
-    const Images = ImageRecSelect.map((value)=>{return value.getAttribute('data-lazy-src')});
-    const uniq = [...new Set(Images)];
+//26.05.24 not needed anymore
+// const alternativeImages = (titles) => {
+//     const ImageRecSelect = [...document.querySelectorAll('.swiper-slide-inner img')];
+//     const Images = ImageRecSelect.map((value)=>{return value.getAttribute('data-lazy-src')});
+//     const uniq = [...new Set(Images)];
 
-    const urls = [];
+//     const urls = [];
 
-    for(let i=0; i<8; i++){
-        let result = filterItems(uniq, titles[i].slice(0,6)); 
-        urls.push(result[0]);
-    }
+//     for(let i=0; i<8; i++){
+//         let result = filterItems(uniq, titles[i].slice(0,6)); 
+//         urls.push(result[0]);
+//     }
 
-    function filterItems(uniq, query) {
-        return uniq.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
-    }
+//     function filterItems(uniq, query) {
+//         return uniq.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+//     }
 
-    return urls
-}
+//     return urls
+// }
 
 const GetImagesRec = (ChromeNodes, titles) => {
 
-    if(window.location.href.includes('produkte')){
-        const imageUrls = alternativeImages(titles);
-        return imageUrls
-    };
+    // if(window.location.href.includes('produkte')){
+    //     const imageUrls = alternativeImages(titles);
+    //     return imageUrls
+    // };
 
     const array = ChromeNodes.map((value)=>{return value.getAttribute('src')});
     const filteredArray = array.filter((word) => (!word.includes('B02') && !word.includes('b02')));
