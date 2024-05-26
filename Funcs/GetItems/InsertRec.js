10a2b40d280efa68a7d2c5a51636f42503fcc133
Michi <mgruener2@gmx.de>
2024-05-26 18:24:07 +0200

    adjusting count for limiting rec itmes

--- Funcs/GetItems/InsertRec.js
+++ Funcs/GetItems/InsertRec.js
@@ -2,6 +2,8 @@ import Items from "./Items.js";
 
 const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type, Id) => {
 
+    let itemamount = 10;
+
         for(let i=0; i <10; i++){
 
             //ignoring if item is are already in basket
@@ -9,16 +11,18 @@ const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type, Id) => {
         if(BasketIDs){
             let DuplicateValue = BasketIDs.filter(BasketID => BasketID.id == Id[i]);
                 if(DuplicateValue[0]){
+                    itemamount -= 1;
                     continue;
                 }
         if(ImageRec[i] != undefined){
             const RecItemsParam = Items(ImageRec[i],titlesRec[i],PriceRec[i],DeeplinkRec[i],Type,Id[i]);
             RecItemsParam.classList = 'RecItem';
+            itemamount -= 1;
         }
 
     }
 }
-    document.querySelector('#recommendationCounter .amount').innerHTML = `${10}`; 
+    document.querySelector('#recommendationCounter .amount').innerHTML = `${itemamount}`; 
 }
 
 export default InsertRec
