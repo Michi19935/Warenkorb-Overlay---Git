10dbbddf2a503fa7d570b2d6466850a8b102efe9
Michi <mgruener2@gmx.de>
2024-05-26 18:20:08 +0200

    limiting rec items on product pages

--- Funcs/GetItems/InsertRec.js
+++ Funcs/GetItems/InsertRec.js
@@ -2,18 +2,19 @@ import Items from "./Items.js";
 
 const InsertRec = (ImageRec, PriceRec, DeeplinkRec, titlesRec, Type, Id) => {
 
-        for(let i=0; i <=10; i++){
+        for(let i=0; i <10; i++){
 
-    //         //ignoring if item is are already in basket
+            //ignoring if item is are already in basket
         let BasketIDs = [...document.querySelectorAll('.BasketItem')];
         if(BasketIDs){
             let DuplicateValue = BasketIDs.filter(BasketID => BasketID.id == Id[i]);
                 if(DuplicateValue[0]){
                     continue;
                 }
-
-        const RecItemsParam = Items(ImageRec[i],titlesRec[i],PriceRec[i],DeeplinkRec[i],Type,Id[i]);
-        RecItemsParam.classList = 'RecItem';
+        if(ImageRec[i] != undefined){
+            const RecItemsParam = Items(ImageRec[i],titlesRec[i],PriceRec[i],DeeplinkRec[i],Type,Id[i]);
+            RecItemsParam.classList = 'RecItem';
+        }
 
     }
 }
